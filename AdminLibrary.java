80a1402a790107706c78cc7c5cf90e601bc5cbbb
ChongLiang924 <chongliang070924@gmail.com>
2026-02-26 14:34:59 +0800

    main

--- AdminLibrary.java
+++ AdminLibrary.java
@@ -1,17 +1,17 @@
 public class AdminLibrary {
-    private String name;
+    private String AdminName;
     private String UserID;
     private String UserType;
-    private String contactNumber;
+    private String Email;;
     private int BorrowLimit; 
     private int CurrentBorrowedBooks;
 
     //Parameterized Constructor
-    public AdminLibrary(String name, String UserID, String UserType, String contactNumber, int BorrowLimit, int CurrentBorrowedBooks) {
-        this.name = name;
+    public AdminLibrary(String AdminName, String UserID, String UserType, String Email, int BorrowLimit, int CurrentBorrowedBooks) {
+        this.AdminName = AdminName;
         this.UserID = UserID;
         this.UserType = UserType;
-        this.contactNumber = contactNumber;
+        this.Email = Email;
         this.BorrowLimit = BorrowLimit;
         this.CurrentBorrowedBooks = CurrentBorrowedBooks;
     }
---
+++ user.java
@@ -0,0 +1,16 @@
+public class user {
+    private String name;
+    private String UserID;
+    private String UserType;
+    private String UserEmail;
+
+    //Parameterized Constructor
+    public user(String name, String UserID, String UserType, String UserEmail) {
+        this.name = name;
+        this.UserID = UserID;
+        this.UserType = UserType;
+        this.UserEmail = UserEmail;
+    }
+
+    
+}
