a6665b403e7dd4e7b373bf34d77324d0049eb2cd
Rick-0813 <leoderrick0813@gmail.com>
2026-03-02 13:19:31 +0800

    new

--- BibiLibrary.java
+++ BibiLibrary.java
@@ -15,24 +15,20 @@ public class BibiLibrary {
             }
             boolean loggedIn = true;
             while (loggedIn) {
-                System.out.println("\n" +
-                    " ____          __               __           __                                              \r\n" +
-                    "/\\  _`\\    __/\\ \\      __     /\\ \\       __/\\ \\                                              \r\n" +
-                    "\\ \\ \\L\\ \\ /\\_\\ \\ \\____/\\_\\    \\ \\ \\     /\\_\\ \\ \\____  _ __    __     _ __   __  __    \r\n" +
-                    " \\ \\  _ <'\\/\\ \\ \\ '__`\\/\\ \\    \\ \\ \\  __\\/\\ \\ \\ '__`\\/\\`'__\\/'__`\\  /\\`'__\\/\\ \\/\\ \\   \r\n" +
-                    "  \\ \\ \\L\\ \\\\ \\ \\ \\ \\L\\ \\ \\ \\    \\ \\ \\L\\ \\\\ \\ \\ \\ \\L\\ \\ \\ \\//\\ \\L\\.\\_\\ \\ \\/ \\ \\ \\_\\ \\  \r\n" +
-                    "   \\ \\____/ \\ \\_\\ \\_,__/\\ \\_\\    \\ \\____/ \\ \\_\\ \\_,__/\\ \\_\\\\ \\__/.\\_\\\\ \\_\\  \\/`____ \\ \r\n" +
-                    "    \\/___/   \\/_/\\/___/  \\/_/     \\/___/   \\/_/\\/___/  \\/_/ \\/__/\\/_/ \\/_/   `/___/> \\\r\n" +
-                    "                                                                                 /\\___/\r\n" +
-                    "                                                                                 \\/__/ ");
+                System.out.println(",-----.  ,--.,--.   ,--.   ,--.   ,--.,--.                                  \r\n" + //
+                                        "|  |) /_ `--'|  |-. `--'   |  |   `--'|  |-. ,--.--. ,--,--.,--.--.,--. ,--.\r\n" + //
+                                        "|  .-.  \\,--.| .-. ',--.   |  |   ,--.| .-. '|  .--'' ,-.  ||  .--' \\  '  / \r\n" + //
+                                        "|  '--' /|  || `-' ||  |   |  '--.|  || `-' ||  |   \\ '-'  ||  |     \\   '  \r\n" + //
+                                        "`------' `--' `---' `--'   `-----'`--' `---' `--'    `--`--'`--'   .-'  /   \r\n" + //
+                                        "                                                                   `---'    ");
                 
-                System.out.println("========================================================================================");
+                System.out.println("=============================================================================");
                 System.out.println("Current Role: " + userType);
                 
                 if (userType.equals("Admin")) {
-                    System.out.println(" 1. User Management\n 2. Audit Inventory\n 3. Reports");
+                    System.out.println(" 1. User Management\n 2. Circulation Module\n 3. Cataloging Admin\n 4. Fees and Audits");
                 } else {
-                    System.out.println(" 1. Search Catalog\n 2. My Loans\n 3. Reservation");
+                    System.out.println(" 1. Search Catalog\n 2. My Loans\n 3. My Bills");
                 }
                 System.out.println(" 0. Logout Back to Main Menu");
                 System.out.print("Choice: ");
@@ -59,8 +55,9 @@ public class BibiLibrary {
         if (userType.equals("Admin")) {
             switch (choice) {
                 case 1: ManagmentUser(); break;
-                case 2: manager.displayAllUsers(); break;
+                case 2: System.out.println("Managing circulation..."); break;
                 case 3: System.out.println("Generating fine reports..."); break;
+                case 4: System.out.println("Performing audits..."); break;
                 default: System.out.println("Invalid choice.");
             }
         } else {
@@ -77,29 +74,46 @@ public class BibiLibrary {
     }
 
     private static void ManagmentUser() {
-        System.out.println("=========================");
-        System.out.println("|User Management Options|");
-        System.out.println("=========================");
-        System.out.println("      1. Add User");
-        System.out.println("     2. Remove User");
-        System.out.println("    3. View All Users");
-        System.out.println("   0. Back to Admin Menu");
+    while (true) {
+        System.out.println("==========================");
+        System.out.println("| User Management Options |");
+        System.out.println("==========================");
+        System.out.println("|      1. Add User       |");
+        System.out.println("|     2. Remove User     |");
+        System.out.println("|    3. View All Users   |");
+        System.out.println("| 4. Account Status Control|");
+        System.out.println("|    0. Back to Admin Menu |");
+        System.out.println("===========================");
         System.out.print("Select an option: ");
+
+        if (!input.hasNextInt()) {
+            System.out.println("Error: Please enter a number.");
+            input.nextLine(); 
+            continue; 
+        }
+
         int userChoice = input.nextInt();
-        input.nextLine();
-        if (userChoice == 1) {
+        input.nextLine(); 
+        //if userchoice is 0, break the loop and return to admin menu
+        if (userChoice == 0) {
+            System.out.println("Returning to Admin Menu...");
+            break;
+        } else if (userChoice == 1) {
             addUser();
         } else if (userChoice == 2) {
             removeUser();
         } else if (userChoice == 3) {
             manager.displayAllUsers();
-        } else if (userChoice == 0) {
-            return;
+        } else if (userChoice == 4) {
+            accountStatusControl();
         } else {
-            System.out.println("Invalid choice.");
+            System.out.println("Invalid choice. Please try again.");
         }
-    }
 
+    }
+}
+
+    
     private static void addUser() {
         
     }
@@ -107,4 +121,8 @@ public class BibiLibrary {
     private static void removeUser() {
 
     }   
+    
+    private static void accountStatusControl() {
+        
+    }
 } 
