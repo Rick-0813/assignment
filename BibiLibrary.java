c33dacbb9d1fc2a86a88804ec72de885fd801e85
Rick-0813 <leoderrick0813@gmail.com>
2026-03-31 19:39:59 +0800

    ok

--- BibiLibrary.java
+++ BibiLibrary.java
@@ -263,11 +263,15 @@ public class BibiLibrary {
 
             else if (choice == 2) {
                 manager.displayAllCatalog();
+<<<<<<< Updated upstream
             }
             
             else if (choice == 3){
                 updateCatologItem();
             }
+=======
+            }            
+>>>>>>> Stashed changes
 
             else if (choice == 4) {
                 System.out.println("\n  .--------------------------------------------------.");
